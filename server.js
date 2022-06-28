fcf527b07a22896358807951ad838b1e727abd1e
ghodgekar <103119421+ghodgekar@users.noreply.github.com>
2022-06-28 21:40:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-// const config=require("./config/config.json")
-// const routes = require('./routes/routes');
-// const port =config.port
+const config=require("./config/config.json");
+const routes = require('./routes/routes');
+const port =config.port;
 
 app.use(function (req,res,next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -11,8 +11,8 @@ app.use(function (req,res,next) {
 });
 
 const PORT = process.env.PORT || 8080;
-// app.use("/",routes);
-app.get('/', (req, res) => {
-    res.send('Hello World, from express');
-});
+app.use("/",routes);
+// app.get('/', (req, res) => {
+//     res.send('Hello World, from express');
+// });
 app.listen(PORT,()=>{ console.log("App Running On Port "+PORT)})
