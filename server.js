b23b7682870ac40ed38eadf8d1c9c8a435705301
ghodgekar <103119421+ghodgekar@users.noreply.github.com>
2022-06-28 21:38:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const config=require("./config/config.json")
-const routes = require('./routes/routes');
-const port =config.port
+// const config=require("./config/config.json")
+// const routes = require('./routes/routes');
+// const port =config.port
 
 app.use(function (req,res,next) {
     res.header("Access-Control-Allow-Origin", "*");
